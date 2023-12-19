907f8229d8b6b1086fe19e1373c65c3978b3de00
Joshuea Pena <josh@macbook-pro.mynetworksettings.com>
2023-12-18 21:41:08 -0500

    day 16

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,3 +1,99 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+
+fn part_1() -> i32 {
+    let hashes = include_str!("./input.txt")
+        .split(",")
+        .map(|str| str.chars().collect_vec())
+        .collect_vec();
+
+    let sum = hashes.iter().fold(0, |mut acc, hash| {
+        let mut current = 0;
+        hash.iter().for_each(|c| {
+            let ascii = *c as u8;
+            current += ascii as i32;
+            current *= 17;
+            current %= 256;
+        });
+        acc += current;
+        acc
+    });
+    sum
+}
+
+fn part_2() -> i32 {
+    let mut map: HashMap<i32, Vec<(&[char], i32)>> = HashMap::new();
+
+    let hashes = include_str!("./input.txt")
+        .split(",")
+        .map(|str| str.chars().collect_vec())
+        .collect_vec();
+
+    hashes.iter().for_each(|hash| {
+        let delimiter_pos = hash.iter().position(|&c| c == '=' || c == '-').unwrap();
+        let (left, right) = hash.split_at(delimiter_pos);
+        let box_number = left.iter().fold(0, |mut acc, c| {
+            acc += (*c as u8) as i32;
+            acc *= 17;
+            acc %= 256;
+            acc
+        });
+        match right[0] {
+            '=' => {
+                let vec = map.get_mut(&box_number);
+                let focal_len = right[1].to_digit(10).unwrap() as i32;
+                match vec {
+                    Some(vec) => {
+                        let is_in_map = vec.iter().position(|(hash, _)| hash == &left);
+                        match is_in_map {
+                            Some(idx) => vec[idx].1 = focal_len,
+                            None => vec.push((left, focal_len)),
+                        }
+                    }
+                    None => {
+                        let mut vec: Vec<_> = Vec::new();
+                        vec.push((left, focal_len));
+                        map.insert(box_number, vec);
+                    }
+                }
+            }
+            '-' => {
+                let vec = map.get_mut(&box_number);
+                match vec {
+                    Some(vec) => {
+                        let idx_to_remove = vec.iter().position(|(hash, _)| hash == &left);
+                        match idx_to_remove {
+                            Some(idx) => {
+                                vec.remove(idx);
+                            }
+                            None => (),
+                        }
+                    }
+                    None => (),
+                }
+            }
+            _ => panic!("wtf is this"),
+        }
+    });
+    map.into_iter().fold(0, |mut acc, (box_num, value)| {
+        acc += value
+            .iter()
+            .enumerate()
+            .fold(0, |mut acc, (i, (_, focal_len))| {
+                acc += (box_num + 1) * (i as i32 + 1) * focal_len;
+                acc
+            });
+        acc
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("part 1: {}", part_1());
+    println!("part 2: {}", part_2());
+}
+
+#[test]
+fn test_1() {
+    assert_eq!(part_1(), 511257);
+    assert_eq!(part_2(), 145);
 }
---
+++ day-16/src/main.rs
@@ -0,0 +1,206 @@
+use itertools::Itertools;
+use rayon::prelude::*;
+use std::collections::{HashMap, VecDeque};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn part_1(start: Point, dir: Point, grid: &Vec<Vec<char>>) -> usize {
+    let mut visited: HashMap<Vec<Point>, u32> = HashMap::new();
+    let mut queue: VecDeque<(Point, Point)> = VecDeque::new();
+    queue.push_back((start, dir));
+    let mut counter = 0;
+
+    while let Some(point) = queue.pop_front() {
+        if counter > 100_000_000 {
+            break;
+        }
+        counter += 1;
+        let (current_point, direction) = point;
+        if current_point.x < 0
+            || current_point.y < 0
+            || current_point.x >= grid[0].len() as i32
+            || current_point.y >= grid.len() as i32
+        {
+            continue;
+        }
+        visited
+            .entry(vec![current_point])
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+        if visited[&vec![current_point]] > 200 {
+            continue;
+        }
+        match grid[current_point.y as usize][current_point.x as usize] {
+            '.' => {
+                let next_point = Point {
+                    x: current_point.x + direction.x,
+                    y: current_point.y + direction.y,
+                };
+                queue.push_back((next_point, direction));
+            }
+            '\\' => {
+                // case direction is left to right
+                if direction.x == 1 && direction.y == 0 {
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y + 1,
+                    };
+                    queue.push_back((next_point, Point { x: 0, y: 1 }));
+                }
+                // case direction is right to left
+                else if direction.x == -1 && direction.y == 0 {
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y + -1,
+                    };
+                    queue.push_back((next_point, Point { x: 0, y: -1 }));
+                }
+                // case direction is up to down
+                else if direction.x == 0 && direction.y == 1 {
+                    let next_point = Point {
+                        x: current_point.x + 1,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, Point { x: 1, y: 0 }));
+                }
+                // case direction is down to up
+                else if direction.x == 0 && direction.y == -1 {
+                    let next_point = Point {
+                        x: current_point.x + -1,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, Point { x: -1, y: 0 }));
+                }
+            }
+            '/' => {
+                // case direction is left to right
+                if direction.x == 1 && direction.y == 0 {
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y + -1,
+                    };
+                    queue.push_back((next_point, Point { x: 0, y: -1 }));
+                }
+                // case direction is right to left
+                else if direction.x == -1 && direction.y == 0 {
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y + 1,
+                    };
+                    queue.push_back((next_point, Point { x: 0, y: 1 }));
+                }
+                // case direction is up to down
+                else if direction.x == 0 && direction.y == 1 {
+                    let next_point = Point {
+                        x: current_point.x + -1,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, Point { x: -1, y: 0 }));
+                }
+                // case direction is down to up
+                else if direction.x == 0 && direction.y == -1 {
+                    let next_point = Point {
+                        x: current_point.x + 1,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, Point { x: 1, y: 0 }));
+                }
+            }
+            '-' => {
+                // horizontal beam, treat as .
+                if direction.y == 0 {
+                    let next_point = Point {
+                        x: current_point.x + direction.x,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, direction));
+                }
+                // vertical beam, split into two
+                else {
+                    let next_point = Point {
+                        x: current_point.x + 1,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, Point { x: 1, y: 0 }));
+                    let next_point = Point {
+                        x: current_point.x - 1,
+                        y: current_point.y,
+                    };
+                    queue.push_back((next_point, Point { x: -1, y: 0 }));
+                }
+            }
+            '|' => {
+                // vertical beam, treat as .
+                if direction.x == 0 {
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y + direction.y,
+                    };
+                    queue.push_back((next_point, direction));
+                }
+                // horizontal beam, split into two
+                else {
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y + 1,
+                    };
+                    queue.push_back((next_point, Point { x: 0, y: 1 }));
+                    let next_point = Point {
+                        x: current_point.x,
+                        y: current_point.y - 1,
+                    };
+                    queue.push_back((next_point, Point { x: 0, y: -1 }));
+                }
+            }
+            _ => panic!("some shit went down"),
+        }
+    }
+    visited.len()
+}
+
+fn main() {
+    let input: &str = include_str!("./input.txt");
+    let grid = input
+        .split("\n")
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+
+    // part_1(Point { x: 0, y: 0 }, Point { x: 1, y: 0 }, &grid);
+
+    let mut starting_points: Vec<(Point, Point)> = Vec::new();
+
+    grid[0].iter().enumerate().for_each(|(i, _)| {
+        // top row going south
+        starting_points.push((Point { x: i as i32, y: 0 }, Point { x: 0, y: 1 }));
+        // bot rows going north
+        starting_points.push((
+            Point {
+                x: i as i32,
+                y: grid.len() as i32 - 1,
+            },
+            Point { x: 0, y: -1 },
+        ));
+    });
+    grid.iter().enumerate().for_each(|(i, _)| {
+        // left col going east
+        starting_points.push((Point { x: 0, y: i as i32 }, Point { x: 1, y: 0 }));
+        // right col going west
+        starting_points.push((
+            Point {
+                x: grid[0].len() as i32 - 1,
+                y: i as i32,
+            },
+            Point { x: -1, y: 0 },
+        ));
+    });
+    let energized = starting_points
+        .par_iter()
+        .map(|(start, dir)| part_1(*start, *dir, &grid))
+        .max()
+        .unwrap();
+    println!("energized {:?}", energized);
+}
